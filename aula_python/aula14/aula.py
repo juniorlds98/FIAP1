a3c41972a5750275b45249c710095628a123d1b6
Junior Silva <150052476+juniorlds98@users.noreply.github.com>
2025-02-24 20:51:26 -0300

    Add files via upload
    Signed-off-by: Junior Silva <150052476+juniorlds98@users.noreply.github.com>

---
+++ aula_python/aula14/aula.py
@@ -0,0 +1,13 @@
+def vogais(texto):
+    vogais = "aeiou"
+    contador = 0
+    for letra in letra.lower():
+        if letras in vogais:
+            contador += 1
+    return contador
+
+def reverter(texto):
+    return texto [::-1]
+
+def contarPalavras(texto):
+    return len(texto.split())
---
+++ aula_python/aula14/aulla.py
@@ -0,0 +1,5 @@
+def sub(x, y):
+    return x- y
+
+a = lambda x, y: x-y
+print(a(2, 3))
